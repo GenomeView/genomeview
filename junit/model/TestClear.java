a01da4ff49a4189930ee16835c81835fea29a6fb
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2009-03-26 20:32:54 +0000

    a small test for the model, needs to be expanded in the future
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@81 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

---
+++ junit/model/TestClear.java
@@ -0,0 +1,54 @@
+package junit.model;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import net.sf.genomeview.data.Model;
+import net.sf.jannot.Entry;
+import net.sf.jannot.exception.ReadFailedException;
+import net.sf.jannot.exception.SaveFailedException;
+import net.sf.jannot.source.DataSource;
+import net.sf.jannot.source.FileSource;
+
+public class TestClear {
+
+	@Test
+	public void testClearEntries() {
+		Model model = new Model(null);
+		try {
+			model.addEntries(new TestDataSource());
+		} catch (ReadFailedException e) {
+
+			e.printStackTrace();
+			Assert.fail();
+		}
+		Assert.assertEquals(5, model.noEntries());
+		model.clearEntries();
+		Assert.assertEquals(0, model.noEntries());
+		
+		
+	}
+
+	class TestDataSource extends DataSource {
+
+		@Override
+		public boolean isDestructiveSave() {
+			return false;
+		}
+
+		@Override
+		public Entry[] read() throws ReadFailedException {
+			Entry[] out = new Entry[5];
+			for (int i = 0; i < out.length; i++)
+				out[i] = new Entry(null);
+			return out;
+		}
+
+		@Override
+		public void saveOwn(Entry[] entries) throws SaveFailedException {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
+}
