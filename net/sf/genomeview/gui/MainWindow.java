9d505c20100a0060bc6fb87bc5cf4a75f963d5d3
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2009-04-22 15:11:04 +0000

    additional windows should not be maximized
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@112 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/MainWindow.java
+++ net/sf/genomeview/gui/MainWindow.java
@@ -212,8 +212,6 @@ public class MainWindow implements WindowListener, Observer {
 				helper.setContentPane(content[i]);
 				helper.setVisible(true);
 				helper.pack();
-				helper.setExtendedState(helper.getExtendedState()
-						+ JFrame.MAXIMIZED_BOTH);
 			}
 		}
 
