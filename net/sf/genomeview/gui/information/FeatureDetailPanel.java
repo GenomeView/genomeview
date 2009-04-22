7fbc8a8caa460060b4e8facec29b0665b377a809
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2009-04-22 22:32:21 +0000

    added calhoun link (only when in the Broad domain)
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@113 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/information/FeatureDetailPanel.java
+++ net/sf/genomeview/gui/information/FeatureDetailPanel.java
@@ -7,15 +7,17 @@ import java.awt.Desktop;
 import java.awt.GridBagConstraints;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
+import java.net.NetworkInterface;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.Enumeration;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Set;
@@ -80,10 +82,11 @@ public class FeatureDetailPanel extends GridBagPanel implements Observer {
 				}
 			}
 
-			/** 
-			 * Class representing a website query. 
+			/**
+			 * Class representing a website query.
+			 * 
 			 * @author Thomas
-			 *
+			 * 
 			 */
 			class Query extends AbstractAction {
 
@@ -92,18 +95,19 @@ public class FeatureDetailPanel extends GridBagPanel implements Observer {
 				 */
 				private static final long serialVersionUID = 5252902483799067615L;
 				private String queryURL;
-//				private String label;
+
+				// private String label;
 
 				Query(String label, String queryURL) {
 					super(label);
-//					this.label = label;
+					// this.label = label;
 					this.queryURL = queryURL;
 				}
 
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					try {
-						String query =queryURL
+						String query = queryURL
 								+ URLEncoder.encode(name.getSelectedText(),
 										"UTF-8");
 
@@ -123,22 +127,61 @@ public class FeatureDetailPanel extends GridBagPanel implements Observer {
 
 			}
 
+			private JPopupMenu popupMenu = null;
+
 			private void showPopUp(MouseEvent e) {
-				JPopupMenu menu = new JPopupMenu();
-				JMenuItem ncbiQuery = new JMenuItem(new Query("Query at NCBI Entrez","http://www.ncbi.nlm.nih.gov/sites/gquery?term="));
-				JMenuItem ensemblQuery = new JMenuItem(new Query("Query at Ensembl","http://www.ensembl.org/Homo_sapiens/Search/Summary?species=all;idx=;q="));
-				JMenuItem ebi = new JMenuItem(new Query("Query at EMBL-EBI","http://www.ebi.ac.uk/ebisearch/search.ebi?db=allebi&query="));
-				
-				JMenuItem google = new JMenuItem(new Query("Query at Google","http://www.google.com/search?q="));
-				
+				if (popupMenu == null) {
+					popupMenu = new JPopupMenu();
+					JMenuItem ncbiQuery = new JMenuItem(new Query(
+							"Query at NCBI Entrez",
+							"http://www.ncbi.nlm.nih.gov/sites/gquery?term="));
+					JMenuItem ensemblQuery = new JMenuItem(
+							new Query("Query at Ensembl",
+									"http://www.ensembl.org/Homo_sapiens/Search/Summary?species=all;idx=;q="));
+					JMenuItem ebi = new JMenuItem(
+							new Query("Query at EMBL-EBI",
+									"http://www.ebi.ac.uk/ebisearch/search.ebi?db=allebi&query="));
 
-					
+					JMenuItem google = new JMenuItem(new Query(
+							"Query at Google",
+							"http://www.google.com/search?q="));
 
-				menu.add(ncbiQuery);
-				menu.add(ensemblQuery);
-				menu.add(ebi);
-				menu.add(google);
-				menu.show(e.getComponent(), e.getX(), e.getY());
+					/* Check whether in Broad domain */
+					boolean inBroadNetwork = false;
+					try {
+
+						Enumeration<NetworkInterface> eni = NetworkInterface
+								.getNetworkInterfaces();
+						while (eni.hasMoreElements()) {
+							NetworkInterface ni = eni.nextElement();
+							// System.out.println(ni.getInetAddresses());
+							Enumeration<InetAddress> eia = ni
+									.getInetAddresses();
+							while (eia.hasMoreElements()) {
+								InetAddress ia = eia.nextElement();
+								System.out.println("Host  : "
+										+ ia.getCanonicalHostName());
+								if (ia.getCanonicalHostName().contains(
+										"broad.mit.edu"))
+									inBroadNetwork = true;
+							}
+						}
+
+					} catch (Exception t) {
+						t.printStackTrace();
+					}
+					JMenuItem calhoun = new JMenuItem(
+							new Query("View feature in Calhoun",
+									"http://calhoun/calhoun/app?page=InspectFeature&service=external&sp=S"));
+
+					popupMenu.add(ncbiQuery);
+					popupMenu.add(ensemblQuery);
+					popupMenu.add(ebi);
+					popupMenu.add(google);
+					if (inBroadNetwork)
+						popupMenu.add(calhoun);
+				}
+				popupMenu.show(e.getComponent(), e.getX(), e.getY());
 
 			}
 
