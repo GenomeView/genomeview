16694dfbfd28d3acb654d713a734d25346dca09e
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2009-08-12 02:38:58 +0000

    Fixed parameterization of cast
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@393 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- org/java/plugin/PluginManager.java
+++ org/java/plugin/PluginManager.java
@@ -93,7 +93,7 @@ public abstract class PluginManager {
         if (obj instanceof Plugin) {
             return ((Plugin) obj).getManager();
         } else if (obj instanceof Class) {
-            clsLoader = ((Class) obj).getClassLoader();
+            clsLoader = ((Class<?>) obj).getClassLoader();
         } else if (obj instanceof ClassLoader) {
             clsLoader = (ClassLoader) obj;
         } else {
