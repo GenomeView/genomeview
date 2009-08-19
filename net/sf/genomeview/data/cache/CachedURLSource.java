b3916a5d550093b9ca1337682ccfa3783d342eeb
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2009-08-19 21:23:42 +0000

    Completed caching implementation, which also fixes the temporary disabling of URLSource
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@467 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/data/cache/CachedURLSource.java
+++ net/sf/genomeview/data/cache/CachedURLSource.java
@@ -4,35 +4,72 @@
 package net.sf.genomeview.data.cache;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.net.URL;
 
 import net.sf.jannot.EntrySet;
 import net.sf.jannot.exception.ReadFailedException;
+import net.sf.jannot.parser.Parser;
 import net.sf.jannot.source.URLSource;
 
-public class CachedURLSource extends URLSource{
+public class CachedURLSource extends URLSource {
 
 	public CachedURLSource(URL url) throws IOException {
 		super(url, null);
-		
+
 	}
 
 	@Override
 	public EntrySet read(EntrySet set) throws ReadFailedException {
 		if (!SourceCache.contains(url)) {
 			try {
-				init();
+				super.setParser(Parser.detectParser(url.openStream()));
+				final PipedInputStream in = new PipedInputStream();
+				final PipedOutputStream forParser = new PipedOutputStream(in);
+
+				new Thread(new Runnable() {
+					public void run() {
+						try {
+							OutputStream out = SourceCache.startCaching(url);
+							InputStream is=url.openStream();
+							byte[] buffer = new byte[100000];
+							while (true) {
+								
+								int amountRead = is.read(buffer);
+								if (amountRead == -1) {
+									break;
+								}
+								forParser.write(buffer, 0, amountRead);
+								out.write(buffer, 0, amountRead);
+
+							}
+							forParser.close();
+							out.close();
+							SourceCache.finish(url);
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					}
+				}).start();
+
+				super.setIos(in);
 			} catch (Exception e) {
 				throw new ReadFailedException(e);
-				
-			} 
-			//Cache the stuff while reading
+
+			}
 			return super.read(set);
-		}else{
-			return SourceCache.get(url).read(set);
+		} else {
+			try {
+				return SourceCache.get(url).read(set);
+			} catch (IOException e) {
+				throw new ReadFailedException(e);
+			}
 		}
 	}
-
-	
 
 }
