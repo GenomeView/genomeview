9f04833910558e00a1e21b47bdf98efc59001c1f
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2009-09-15 21:55:45 +0000

    added progress window when downloading BAM/BAI data
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@568 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/data/cache/SAMCache.java
+++ net/sf/genomeview/data/cache/SAMCache.java
@@ -18,6 +18,8 @@ import be.abeel.io.ExtensionFileFilter;
 import be.abeel.security.MD5Tools;
 
 import net.sf.genomeview.core.Configuration;
+import net.sf.genomeview.gui.StaticUtils;
+import net.sf.genomeview.gui.dialog.GVProgressBar;
 import net.sf.jannot.source.DataSource;
 import net.sf.jannot.source.FileSource;
 import net.sf.jannot.source.SAMDataSource;
@@ -45,32 +47,41 @@ public class SAMCache {
 	public static SAMDataSource getSAM(URL url) throws IOException {
 		String md5 = MD5Tools.md5(url.toString())+".bam" ;
 		if (!contains(md5)) {
+			GVProgressBar gv=new GVProgressBar("Downloading data","Downloading short read data",null);
+			StaticUtils.upperRight(gv);
+			
 			File tmpBAM = new File(cacheDir, md5+".tmp");
 			File tmpBAI = new File(cacheDir, md5 + ".bai"+".tmp");
 			URLConnection conn = url.openConnection();
 			System.out.println("Downloading: " + conn.getContentLength() + " bytes");
-			copy(conn.getInputStream(), tmpBAM);
-			copy(new URL(url.toString() + ".bai").openStream(), tmpBAI);
+			gv.setMax(conn.getContentLength()/1024);
+			gv.setVisible(true);
+			copy(conn.getInputStream(), tmpBAM,gv);
+			copy(new URL(url.toString() + ".bai").openStream(), tmpBAI,null);
 			tmpBAM.renameTo(new File(cacheDir, md5));
 			tmpBAI.renameTo(new File(cacheDir, md5 + ".bai"));
+			gv.done();
 
 		}
 		return new SAMDataSource(new File(cacheDir, md5));
 	}
 
-	private static void copy(InputStream in, File file) throws IOException {
+	private static void copy(InputStream in, File file, GVProgressBar gv) throws IOException {
 		OutputStream out = new FileOutputStream(file);
 
 		byte[] buffer = new byte[100000];
+		long read=0;
 		while (true) {
 			int amountRead = in.read(buffer);
 			if (amountRead == -1) {
 				break;
 			}
 			out.write(buffer, 0, amountRead);
-
+			read+=amountRead;
+			if(gv!=null)
+				gv.setProgress((int)(read/1024));
 		}
 		out.close();
-
+		
 	}
 }
