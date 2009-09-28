67cc549fb4ab9ebfac78b87fc8b534c4fa989976
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2009-09-28 22:27:05 +0000

    Fixed some bugs in wiggle track
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@648 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/annotation/track/WiggleTrack.java
+++ net/sf/genomeview/gui/annotation/track/WiggleTrack.java
@@ -5,15 +5,23 @@ package net.sf.genomeview.gui.annotation.track;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.geom.GeneralPath;
 
 import net.sf.genomeview.data.Model;
+import net.sf.genomeview.gui.Convert;
+import net.sf.genomeview.gui.StaticUtils;
 import net.sf.jannot.Entry;
+import net.sf.jannot.Location;
+import net.sf.jannot.Strand;
+import net.sf.jannot.shortread.ReadGroup;
+import net.sf.jannot.shortread.ShortReadCoverage;
 import net.sf.jannot.wiggle.Graph;
 
 // A SINGLE WIGGLE TRACK CAN CONTAIN MULTIPLE GRAPHS
 public class WiggleTrack extends Track {
 
 	private String name;
+	private Location currentVisible;
 
 	public WiggleTrack(String name, Model model, boolean b) {
 		super(model, b,true);
@@ -29,12 +37,59 @@ public class WiggleTrack extends Track {
 
 	@Override
 	public int paintTrack(Graphics2D g, Entry e, int yOffset, double screenWidth) {
-		
+		currentVisible = model.getAnnotationLocationVisible();
+		int graphLineHeigh=50;
 		/* keeps track of the space used during painting */
 		int yUsed = 0;
 		Graph graph = e.graphs.getGraph(name);
 		if (graph != null) {
+			double width = screenWidth / (double) currentVisible.length() / 2.0;
 
+			int scale = 1;
+			int scaleIndex = 0;
+			while (scale < (int) Math.ceil(1.0 / width)) {
+				scale *= 2;
+				scaleIndex++;
+			}
+
+			int start = currentVisible.start / scale * scale;
+			int end = ((currentVisible.end / scale) + 1) * scale;
+		
+			GeneralPath conservationGP = new GeneralPath();
+		
+			float[] f = graph.get(start-1, end, scaleIndex);
+			float[] r = graph.get(start-1, end, scaleIndex);
+			
+			for (int i = 0; i < f.length; i++) {
+				int x = Convert.translateGenomeToScreen(start + i * scale, currentVisible, screenWidth);
+				double valF = f[i];
+				double valR = r[i];
+				double val = f[i] + r[i];
+				/* Cap value */
+				if (valF > graph.max())
+					valF = graph.max();
+				if (valR > graph.max())
+					valR = graph.max();
+				if (val > graph.max())
+					val = graph.max();
+				
+				val-=graph.min();
+				val /= graph.max()-graph.min();
+
+				/* Draw lines */
+				if (i == 0) {
+					conservationGP.moveTo(x - 1, yOffset + (1 - val) * (graphLineHeigh - 4) + 2);
+				}
+
+				conservationGP.lineTo(x, yOffset + (1 - val) * (graphLineHeigh - 4) + 2);
+
+			}
+
+			g.setColor(Color.BLACK);
+			g.draw(conservationGP);
+
+			
+			// }
 //			SortedMap<Location, Double> vf = graph.get(model.getAnnotationLocationVisible());
 //
 //			int lastX = -1;
@@ -87,8 +142,8 @@ public class WiggleTrack extends Track {
 //			}
 
 			g.setColor(Color.black);
-			g.drawString(graph.getName(), 10, yOffset + yUsed);
-
+			g.drawString(graph.getName(), 10, yOffset + yUsed+15);
+			yUsed += graphLineHeigh;
 		}
 		
 		return yUsed;
