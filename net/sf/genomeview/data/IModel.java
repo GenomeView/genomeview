1cc87e37ad6a59129bb7f68ffcc443816e7c2d8f
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2009-11-24 23:15:35 +0000

    expose the entries() method to plugins
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@832 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/data/IModel.java
+++ net/sf/genomeview/data/IModel.java
@@ -10,6 +10,7 @@ import javax.swing.JFrame;
 import net.sf.genomeview.gui.annotation.track.Track;
 import net.sf.genomeview.plugin.GUIManager;
 import net.sf.jannot.Entry;
+import net.sf.jannot.EntrySet;
 import net.sf.jannot.Location;
 
 /**
@@ -20,28 +21,35 @@ import net.sf.jannot.Location;
  */
 public interface IModel extends Observer {
 
-    /**
-     * Returns the entry that is currently selected.
-     * 
-     * @return the entry that is currently selected
-     */
-    public Entry getSelectedEntry();
+	/**
+	 * Returns the entry that is currently selected.
+	 * 
+	 * @return the entry that is currently selected
+	 */
+	public Entry getSelectedEntry();
 
-    /**
-     * Returns the region that is currently selected. If nothin is selected, this method will return null.
-     * 
-     * @return the region of the sequence that is currently selected.
-     */
-    public Location getSelectedRegion();
+	/**
+	 * Returns the region that is currently selected. If nothin is selected,
+	 * this method will return null.
+	 * 
+	 * @return the region of the sequence that is currently selected.
+	 */
+	public Location getSelectedRegion();
 
-    public JFrame getParent();
+	public JFrame getParent();
 
-    public GUIManager getGUIManager();
-    
-    public void addTrack(Track track);
+	public GUIManager getGUIManager();
+
+	public void addTrack(Track track);
 
 	public void updateTracks();
 
 	public void addObserver(Observer observer);
 
+	/**
+	 * Returns the entire data set that is currently loaded in GenomeView.
+	 * 
+	 * @return
+	 */
+	public EntrySet entries();
 }
