bf8f29bb0610f3ba53ea21604a8708bf25749443
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2010-08-12 13:38:07 +0000

    updated splash screen
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1041 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/Splash.java
+++ net/sf/genomeview/gui/Splash.java
@@ -3,25 +3,40 @@
  */
 package net.sf.genomeview.gui;
 
-import java.awt.Dimension;
+import java.awt.Color;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
-import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JWindow;
+import javax.swing.border.Border;
+/**
+ * 
+ * @author Thomas Abeel
+ *
+ */
+public class Splash extends JWindow {
 
-public class Splash extends JFrame {
+	private static final long serialVersionUID = 2469220030126994849L;
 
-    private static final long serialVersionUID = 2469220030126994849L;
+	public Splash() {
+		super();
+		floater = new JLabel(new ImageIcon(this.getClass().getResource("/images/splash.png")));
+		Rectangle bounds = GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds();
+		this.setAlwaysOnTop(true);
+		Border emptyBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
+		Border colorBorder = BorderFactory.createLineBorder(Color.BLACK);
+		floater.setBorder(BorderFactory.createCompoundBorder(colorBorder, emptyBorder));
+		add(floater);
+		pack();
+		this.setLocation(bounds.x + bounds.width/2 -floater.getWidth()/2, bounds.y + bounds.height /2 -floater.getHeight()/2);
+		setVisible(true);
+	
+	}
 
-    public Splash() {
-        super();
-        getContentPane().add(new JLabel(new ImageIcon(this.getClass().getResource("/images/splash.png"))));
-        // setAlwaysOnTop(true);
-        setUndecorated(true);
-        setVisible(true);
-        pack();
-        Dimension ss = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
-        setLocation(ss.width / 2 - getPreferredSize().width / 2, ss.height / 2 - getPreferredSize().height / 2);
+	private JLabel floater = new JLabel();
 
-    }
+	
 }
