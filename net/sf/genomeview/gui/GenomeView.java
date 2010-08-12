9ab35ad14cfec50c95cf0a54fb9686620c407cb6
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2010-08-12 13:28:40 +0000

    Removed MyAuthenticator code, execute GUI code on EventThread
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1038 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/GenomeView.java
+++ net/sf/genomeview/gui/GenomeView.java
@@ -3,86 +3,24 @@
  */
 package net.sf.genomeview.gui;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.net.Authenticator;
-import java.net.PasswordAuthentication;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
 
 import net.sf.genomeview.core.Configuration;
-import be.abeel.gui.TitledComponent;
-
+/**
+ * 
+ * @author Thomas Abeel
+ *
+ */
 public class GenomeView {
 	private static Logger logger;
-
-	// public static GenomeView single;
-
-	static private class MyAuthenticator extends Authenticator {
-
-		private String user = "";
-		private char[] pass = new char[0];
-
-		protected PasswordAuthentication getPasswordAuthentication() {
-			final JDialog jd = new JDialog();
-			ActionListener dps = new ActionListener() {
-
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					jd.dispose();
-
-				}
-			};
-			
-			jd.setTitle("Enter password");
-			jd.setModal(true);
-			jd.setLayout(new GridBagLayout());
-			GridBagConstraints gc = new GridBagConstraints();
-			gc.gridx = 0;
-			gc.gridy = 0;
-			gc.fill = GridBagConstraints.BOTH;
-
-			JLabel jl = new JLabel("Please enter login details for: " + getRequestingPrompt() + " at "
-					+ getRequestingHost());
-			jd.add(jl, gc);
-			gc.gridy++;
-			JTextField username = new JTextField(user);
-			username.addActionListener(dps);
-			jd.add(new TitledComponent("User name", username), gc);
-			gc.gridy++;
-			JPasswordField password = new JPasswordField(new String(pass));
-			password.addActionListener(dps);
-			jd.add(new TitledComponent("Password", password), gc);
-			gc.gridy++;
-			JButton jb = new JButton("OK");
-			jb.addActionListener(dps);
-			jd.add(jb, gc);
-			
-			jd.pack();
-			StaticUtils.center(jd);
-			jd.setVisible(true);
-			logger.info("Requesting Host  : " + getRequestingHost());
-			logger.info("Requesting Port  : " + getRequestingPort());
-			logger.info("Requesting Prompt : " + getRequestingPrompt());
-			logger.info("Requesting Protocol: " + getRequestingProtocol());
-			logger.info("Requesting Scheme : " + getRequestingScheme());
-			logger.info("Requesting Site  : " + getRequestingSite());
-			this.user = username.getText();
-			this.pass = password.getPassword();
-			return new PasswordAuthentication(this.user, this.pass);
-		}
-	}
 
 	private static MainWindow mw;
 
@@ -94,78 +32,99 @@ public class GenomeView {
 			args[0] = "--file";
 
 	}
+	private static Splash splash=null;
 
 	public static void main(final String[] args) {
-		/* Rewrite JNLP arguments */
-		jnlprewrite(args);
-		/*
-		 * The configuration class needs to be called at least once before we
-		 * can start the logger
-		 */
-		System.out.println("Starting GenomeView " + Configuration.version());
-
-		/* Configure logging */
 		try {
-			LogManager.getLogManager().readConfiguration(GenomeView.class.getResourceAsStream("/conf/logging.conf"));
-			logger = Logger.getLogger(GenomeView.class.getCanonicalName());
-		} catch (SecurityException e) {
-			logger.log(Level.SEVERE, "log initialization", e);
-			/*
-			 * These exceptions likely indicate that the log could not be
-			 * initialized, print also to console
-			 */
-			e.printStackTrace();
+			SwingUtilities.invokeAndWait(new Runnable() {
 
-		} catch (IOException e) {
-			logger.log(Level.SEVERE, "log initialization", e);
-			/*
-			 * These exceptions likely indicate that the log could not be
-			 * initialized, print also to console
-			 */
-			e.printStackTrace();
-		}
-
-		/* Single instance manager */
-		boolean singleInstance = Configuration.getBoolean("general:singleInstance");
-		if (singleInstance) {
-			if (!ApplicationInstanceManager.registerInstance()) {
-				// instance already running.
-				System.out.println("Another instance of this application is already running.  Exiting.");
-				System.exit(0);
-			}
-			ApplicationInstanceManager.setApplicationInstanceListener(new ApplicationInstanceListener() {
-				public void newInstanceCreated() {
-					System.out.println("New instance detected...");
-					try {
-						assert mw!=null;
-						mw.init(args);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (ExecutionException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+				@Override
+				public void run() {
+					
+					splash = new Splash();
 				}
 			});
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (InvocationTargetException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
+		
+		
+		SwingUtilities.invokeLater(new Runnable() {
 
-		Authenticator.setDefault(new MyAuthenticator());
+			@Override
+			public void run() {
+				/* Rewrite JNLP arguments */
+				jnlprewrite(args);
+				/*
+				 * The configuration class needs to be called at least once
+				 * before we can start the logger
+				 */
+				System.out.println("Starting GenomeView " + Configuration.version());
 
-		try {
-			mw = new MainWindow(args);
+				/* Configure logging */
+				try {
+					LogManager.getLogManager().readConfiguration(
+							GenomeView.class.getResourceAsStream("/conf/logging.conf"));
+					logger = Logger.getLogger(GenomeView.class.getCanonicalName());
+				} catch (SecurityException e) {
+					logger.log(Level.SEVERE, "log initialization", e);
+					/*
+					 * These exceptions likely indicate that the log could not
+					 * be initialized, print also to console
+					 */
+					e.printStackTrace();
 
-		} catch (InterruptedException e) {
-			logger.log(Level.SEVERE, "main window initialization", e);
-		} catch (ExecutionException e) {
-			logger.log(Level.SEVERE, "main window initialization", e);
-		}
-		splash.dispose();
+				} catch (IOException e) {
+					logger.log(Level.SEVERE, "log initialization", e);
+					/*
+					 * These exceptions likely indicate that the log could not
+					 * be initialized, print also to console
+					 */
+					e.printStackTrace();
+				}
+
+				/* Single instance manager */
+				boolean singleInstance = Configuration.getBoolean("general:singleInstance");
+				if (singleInstance) {
+					if (!ApplicationInstanceManager.registerInstance()) {
+						// instance already running.
+						System.out.println("Another instance of this application is already running.  Exiting.");
+						System.exit(0);
+					}
+					ApplicationInstanceManager.setApplicationInstanceListener(new ApplicationInstanceListener() {
+						public void newInstanceCreated() {
+							System.out.println("New instance detected...");
+							try {
+								assert mw != null;
+								mw.init(args);
+							} catch (InterruptedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (ExecutionException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+					});
+				}
+
+				Authenticator.setDefault(new MyAuthenticator());
+
+				try {
+					mw = new MainWindow(args);
+
+				} catch (InterruptedException e) {
+					logger.log(Level.SEVERE, "main window initialization", e);
+				} catch (ExecutionException e) {
+					logger.log(Level.SEVERE, "main window initialization", e);
+				}
+				splash.dispose();
+			}
+		});
 	}
-
-	/*
-	 * This will create AND SHOW the Splash screen.
-	 */
-	private static Splash splash = new Splash();
 
 }
