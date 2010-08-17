ea810e90cc415a6def7e6a07df8ffeedacc8e8ac
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2010-08-17 12:00:21 +0000

    fixed NPE when clicking outside a track
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1052 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/menu/PopUpMenu.java
+++ net/sf/genomeview/gui/menu/PopUpMenu.java
@@ -27,81 +27,79 @@ import net.sf.genomeview.gui.menu.selection.ShowSequenceWindowAction;
 import net.sf.jannot.Feature;
 import net.sf.jannot.Qualifier;
 
-
 public class PopUpMenu extends JPopupMenu {
 
-    private static final long serialVersionUID = 2573433669184123608L;
+	private static final long serialVersionUID = 2573433669184123608L;
 
-    private int count=0;
-    public PopUpMenu(Model model,Track t) {
-    	/* Track specific actions */
-    	List<JMenuItem>list=t.getMenuItems();
-    	for(JMenuItem a:list){
-    		if(a.isEnabled()){
-    			count++;
-    			add(a);
-    		}
-    	}
-    	if(count>0)
-    		addSeparator();
-    	
-    	/* Other actions */
-        addC(new RemoveAction(model));
-        addC(new EditStructureAction(model));
-        addC(new ClearFeatureSelectionAction(model));
-        addC(new ShowSequenceWindowAction(model));
-        if(count>0)
-        	addSeparator();
+	private int count = 0;
 
-        addC(new ClearRegionSelectionAction(model));
+	public PopUpMenu(Model model, Track t) {
+		/* Track specific actions, if there is a track */
+		if (t != null) {
+			List<JMenuItem> list = t.getMenuItems();
+			for (JMenuItem a : list) {
+				if (a.isEnabled()) {
+					count++;
+					add(a);
+				}
+			}
+			if (count > 0)
+				addSeparator();
+		}
+		count=0;
+		/* Other actions */
+		addC(new RemoveAction(model));
+		addC(new EditStructureAction(model));
+		addC(new ClearFeatureSelectionAction(model));
+		addC(new ShowSequenceWindowAction(model));
+		if (count > 0)
+			addSeparator();
+		count=0;
+		addC(new ClearRegionSelectionAction(model));
 
-        addC(new CreateNewFeatureAction(model));
-        addC(new CloneFeatureAction(model));
-        addC(new MergeFeatureAction(model));
-        addC(new SplitFeatureAction(model));
-        
-        SortedSet<Feature>sf=model.selectionModel().getFeatureSelection();
-        List<Action>actions=new ArrayList<Action>();
-        for(Feature f:sf){
-        	List<Qualifier>lq=f.qualifier("url");
-        	for(Qualifier q:lq){
-        		String name=q.getValue().split(":")[0];
-        		String url=q.getValue().substring(q.getValue().indexOf(':')+1);
-        		actions.add(new OpenURLAction(name,url));
-        	}
-        }
-        if(actions.size()>0)
-        	addSeparator();
-        for(Action a:actions)
-        	add(a);
-        count+=actions.size();
-        if(count==0){
-        	add(new AbstractAction("No actions available"){
+		addC(new CreateNewFeatureAction(model));
+		addC(new CloneFeatureAction(model));
+		addC(new MergeFeatureAction(model));
+		addC(new SplitFeatureAction(model));
 
-        	
-				
+		SortedSet<Feature> sf = model.selectionModel().getFeatureSelection();
+		List<Action> actions = new ArrayList<Action>();
+		for (Feature f : sf) {
+			List<Qualifier> lq = f.qualifier("url");
+			for (Qualifier q : lq) {
+				String name = q.getValue().split(":")[0];
+				String url = q.getValue().substring(q.getValue().indexOf(':') + 1);
+				actions.add(new OpenURLAction(name, url));
+			}
+		}
+		if (actions.size() > 0)
+			addSeparator();
+		for (Action a : actions)
+			add(a);
+		count += actions.size();
+		if (count == 0) {
+			add(new AbstractAction("No actions available") {
+
 				@Override
 				public boolean isEnabled() {
 					return false;
 				}
 
 				public void actionPerformed(ActionEvent e) {
-									
-				}
-        	
-        	
-        	});
-        }
-        
 
-    }
+				}
+
+			});
+		}
+
+	}
 
 	private void addC(Action a) {
-		if(a.isEnabled()){
+		if (a.isEnabled()) {
 			count++;
 			add(a);
 		}
-		
+
 	}
 
 }
