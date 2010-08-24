e816bf6f07414e13b23623e881a93bc2532550b0
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2010-08-24 14:40:25 +0000

    working component integration
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1116 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- devtools/ascomponent/GenomeViewAsComponent.java
+++ devtools/ascomponent/GenomeViewAsComponent.java
@@ -1,24 +1,40 @@
 package devtools.ascomponent;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import net.sf.genomeview.data.DataSourceFactory;
 import net.sf.genomeview.data.Model;
 import net.sf.genomeview.gui.MainContent;
+import net.sf.genomeview.gui.task.ReadWorker;
+import net.sf.jannot.source.DataSource;
 
 public class GenomeViewAsComponent {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException, IOException {
 		JFrame frame = new JFrame("GenomeView as component demo");
 		Model model=new Model(frame);
 		JPanel[] content=MainContent.createContent(model,1);
 		frame.setContentPane(content[0]);
 		frame.pack();
 		frame.setVisible(true);
-		model.addData(f)
-	    * http://www.broadinstitute.org/software/genomeview/demo_c_elegans/IV.fasta.gz
-	        * http://www.broadinstitute.org/software/genomeview/demo_c_elegans/IV.gff.gz
-	        * http://www.broadinstitute.org/software/genomeview/demo_c_elegans/uwgs-rw_L2_FC6218_3.CHROMOSOME_IV.sorted.bam.bai
+		
+		DataSource ds = DataSourceFactory.createURL(new URL("http://www.broadinstitute.org/software/genomeview/demo_c_elegans/IV.fasta.gz"));
+		ReadWorker rf = new ReadWorker(ds, model);
+		rf.execute();
+		
+		ds = DataSourceFactory.createURL(new URL("http://www.broadinstitute.org/software/genomeview/demo_c_elegans/IV.gff.gz"));
+		rf = new ReadWorker(ds, model);
+		rf.execute();
+		
+		ds = DataSourceFactory.createURL(new URL(" http://www.broadinstitute.org/software/genomeview/demo_c_elegans/uwgs-rw_L2_FC6218_3.CHROMOSOME_IV.sorted.bam.bai"));
+		rf = new ReadWorker(ds, model);
+		rf.execute();
+	    
 
 	}
 }
