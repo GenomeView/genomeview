62c369a523877f590966fba6454b2ed52c0a4ab9
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2010-08-25 14:33:45 +0000

    saving to URL should in theory work again
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1131 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/dialog/SaveDialog.java
+++ net/sf/genomeview/gui/dialog/SaveDialog.java
@@ -10,20 +10,29 @@ import java.awt.GridLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Map.Entry;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 
-import be.abeel.gui.TitledComponent;
-
+import net.sf.genomeview.core.Configuration;
+import net.sf.genomeview.data.ClientHttpUpload;
 import net.sf.genomeview.data.Model;
 import net.sf.genomeview.gui.StaticUtils;
-import net.sf.jannot.Data;
-import net.sf.jannot.DataKey;
+import net.sf.jannot.Entry;
+import net.sf.jannot.exception.SaveFailedException;
+import net.sf.jannot.parser.EMBLParser;
+import be.abeel.io.ExtensionManager;
+import be.abeel.io.LineIterator;
 
 /**
  * 
@@ -38,11 +47,11 @@ public class SaveDialog extends JDialog {
 
 		private static final long serialVersionUID = -208816638301437642L;
 
-		private Data<?> data;
+		private Entry data;
 
-		public DataSourceCheckbox(Data<?> data) {
-			super(data.toString());
-			this.data = data;
+		public DataSourceCheckbox(Entry e) {
+			super(e.toString());
+			this.data = e;
 		}
 
 	}
@@ -57,30 +66,39 @@ public class SaveDialog extends JDialog {
 		gc.gridheight = 1;
 		gc.gridx = 0;
 		gc.gridy = 0;
+		gc.weightx = 1;
+		gc.weighty = 1;
+		gc.fill = GridBagConstraints.BOTH;
 
 		final ArrayList<DataSourceCheckbox> dss = new ArrayList<DataSourceCheckbox>();
 		add(new JLabel("Select sources to save"), gc);
+		// Container all = new Container();
+		// all.setLayout(new GridLayout(0, 1));
 		gc.gridy++;
+		Container cp = new Container();
+		cp.setLayout(new GridLayout(0, 1));
 		for (net.sf.jannot.Entry e : model.entries()) {
-			Container cp = new Container();
-			cp.setLayout(new GridLayout(0, 1));
-			int count = 0;
-			for (DataKey ds : e) {
-				Data s = e.get(ds);
-				if (s.canSave()) {
-					DataSourceCheckbox dsb = new DataSourceCheckbox(s);
-					dss.add(dsb);
-					cp.add(dsb);
-					count++;
 
-				}
-			}
-			if (count > 0) {
-				add(new TitledComponent(e.toString(), cp),gc);
-				gc.gridy++;
-			}
+			// int count = 0;
+			DataSourceCheckbox dsb = new DataSourceCheckbox(e);
+			dsb.setSelected(true);
+			// for (DataKey ds : e) {
+			// Data s = e.get(ds);
+			// if (s.canSave()) {
+			//					
+			dss.add(dsb);
+			cp.add(dsb);
+			// count++;
+			//
+			// }
+			// }
+			// if (count > 0) {
+			// all.add(new TitledComponent(e.toString(), cp));
+			//
+			// }
 		}
-
+		add(new JScrollPane(cp), gc);
+		gc.gridy++;
 		JButton save = new JButton("Save");
 		JButton close = new JButton("Close");
 
@@ -90,54 +108,98 @@ public class SaveDialog extends JDialog {
 		gc.gridx++;
 		add(close, gc);
 
-		// save.addActionListener(new ActionListener() {
-		//
-		// @Override
-		// public void actionPerformed(ActionEvent e) {
-		//
-		// for (DataSourceCheckbox dsb : dss) {
-		// if (dsb.isSelected()) {
-		// boolean continueSave = true;
-		// if (dsb.data.isDestructiveSave()) {
-		// int result =
-		// JOptionPane.showConfirmDialog(model.getGUIManager().getParent(),
-		// "Overwrite existing file?\n"
-		// + dsb.data, "Overwrite?", JOptionPane.YES_NO_OPTION);
-		// if (result != JOptionPane.YES_OPTION)
-		// continueSave = false;
-		// }
-		//
-		// if (continueSave) {
-		// boolean seqSaved = false;
-		// for (Entry en : model.entries()) {
-		// if (en.defaultSource.equals(dsb.data))
-		// seqSaved = true;
-		//
-		// }
-		// if (!seqSaved) {
-		// int result =
-		// JOptionPane.showConfirmDialog(model.getGUIManager().getParent(),
-		// "Save source without sequence?", "No sequence!",
-		// JOptionPane.YES_NO_OPTION);
-		// if (result != JOptionPane.YES_OPTION)
-		// continueSave = false;
-		// }
-		// }
-		// setVisible(false);
-		// if (continueSave) {
-		// WriteEntriesWorker rw = new WriteEntriesWorker(dsb.data, model);
-		// rw.execute();
-		// } else {
-		// JOptionPane.showMessageDialog(model.getGUIManager().getParent(),
-		// "Save aborted!");
-		// }
-		//
-		// }
-		// }
-		//
-		// }
-		//
-		// });
+		save.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					/* Default save locations? */
+					String defaultLocation = Configuration.get("save:defaultLocation");
+					if (defaultLocation.equals("null")) {
+						defaultLocation = file();
+					}
+					if (defaultLocation == null) {
+						return;
+					}
+					EMBLParser parser = new EMBLParser();
+					parser.storeSequence = false;
+					File tmp = File.createTempFile("GV_", ".save");
+					tmp.deleteOnExit();
+
+					FileOutputStream fos = new FileOutputStream(tmp);
+
+					for (DataSourceCheckbox dsb : dss) {
+						if (dsb.isSelected()) {
+							parser.write(fos, dsb.data);
+							// for (Feature f : (Iterable<Feature>)
+							// dsb.data.get()) {
+							// pw.println(parser.line(f));
+							// }
+						}
+					}
+					fos.close();
+					setVisible(false);
+					if (defaultLocation.startsWith("http://") || defaultLocation.startsWith("https://")) {
+						try {
+							URL url=new URL(defaultLocation);
+							System.out.println(url.getProtocol() + "://" + url.getHost() + url.getPath());
+							url = new URL(url.getProtocol() + "://" + url.getHost() + url.getPath());
+							
+							LineIterator it = new LineIterator(tmp);
+							System.out.println("-------");
+							System.out.println("Uploaded file:");
+							for (String line : it)
+								System.out.println(line);
+							System.out.println("---EOF---");
+							it.close();
+
+							String reply = ClientHttpUpload.upload(tmp, url);
+							System.out.println("SERVER REPLY: " + reply);
+							// TODO add more checks on the reply.
+							if (reply.equals("")) {
+
+								throw new SaveFailedException("Empty reply from server");
+
+							}
+							if (reply.toLowerCase().contains("error")) {
+								JOptionPane.showMessageDialog(null, reply);
+								throw new SaveFailedException("Error reply from server");
+
+							}
+							JOptionPane.showMessageDialog(model.getGUIManager().getParent(), reply);
+
+						} catch (IOException ex) {
+
+							ex.printStackTrace();
+							throw new SaveFailedException("IOException");
+						}
+					} else {
+						File out = new File(defaultLocation);
+						out = ExtensionManager.extension(out, "embl");
+						boolean succes = tmp.renameTo(out);
+						if (!succes) {
+							JOptionPane.showMessageDialog(model.getGUIManager().getParent(), "Save failed!");
+						} else {
+							JOptionPane.showMessageDialog(model.getGUIManager().getParent(), "Save succeeded!");
+						}
+					}
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+
+			private String file() {
+				JFileChooser chooser = new JFileChooser(Configuration.getFile("lastDirectory"));
+				int returnVal = chooser.showSaveDialog(model.getGUIManager().getParent());
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					File files = chooser.getSelectedFile();
+					return files.toString();
+				} else {
+					return null;
+				}
+			}
+
+		});
 
 		close.addActionListener(new ActionListener() {
 
@@ -149,8 +211,8 @@ public class SaveDialog extends JDialog {
 
 		});
 
-		StaticUtils.center(this);
 		pack();
+		StaticUtils.center(this);
 		setVisible(true);
 	}
 
