d6caa96f33fb1d9e7e061f00c07eb0218773ed94
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2010-08-25 12:53:29 +0000

    made backward compatible with PHP4.x
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1129 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- devtools/integration/applet.php
+++ devtools/integration/applet.php
@@ -1,3 +1,30 @@
+<?php
+/* PHP 4.x compatibility */
+if(!function_exists("stripos")){
+    function stripos(  $str, $needle, $offset = 0  ){
+        return strpos(  strtolower( $str ), strtolower( $needle ), $offset  );
+    }/* endfunction stripos */
+}/* endfunction exists stripos */
+
+if(!function_exists("strripos")){
+    function strripos(  $haystack, $needle, $offset = 0  ) {
+        if(  !is_string( $needle )  )$needle = chr(  intval( $needle )  );
+        if(  $offset < 0  ){
+            $temp_cut = strrev(  substr( $haystack, 0, abs($offset) )  );
+        }
+        else{
+            $temp_cut = strrev(    substr(   $haystack, 0, max(  ( strlen($haystack) - $offset ), 0  ) 
+
+  )    );
+        }
+        if(   (  $found = stripos( $temp_cut, strrev($needle) )  ) === FALSE   )return FALSE;
+        $pos = (   strlen(  $haystack  ) - (  $found + $offset + strlen( $needle )  )   );
+        return $pos;
+    }/* endfunction strripos */
+}/* endfunction exists strripos */
+?>
+
+
 <html>
 <head>
 <title>GenomeView Applet</title>
