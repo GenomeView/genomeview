83bdea2b434ed885248eb760b82083fed9a913e6
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2010-09-09 08:00:18 +0000

    fixed 3053527
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1144 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/menu/edit/RemoveLocationAction.java
+++ net/sf/genomeview/gui/menu/edit/RemoveLocationAction.java
@@ -12,35 +12,52 @@ import java.util.SortedSet;
 
 import net.sf.genomeview.data.Model;
 import net.sf.genomeview.gui.menu.AbstractModelAction;
+import net.sf.jannot.Data;
+import net.sf.jannot.Feature;
 import net.sf.jannot.Location;
+import net.sf.jannot.MemoryFeatureAnnotation;
+
 /**
  * 
  * @author Thomas Abeel
- *
+ * 
  */
 public class RemoveLocationAction extends AbstractModelAction implements Observer {
 
-    private static final long serialVersionUID = -5857913546086864524L;
+	private static final long serialVersionUID = -5857913546086864524L;
 
-    public RemoveLocationAction(Model model) {
-        super("Remove selected location", model);
-    }
+	public RemoveLocationAction(Model model) {
+		super("Remove selected location", model);
+	}
 
-    public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent arg0) {
 
-        Set<Location> toRemove = new HashSet<Location>();
-        toRemove.addAll(model.selectionModel().getLocationSelection());
-        model.selectionModel().clearLocationSelection();
-        for (Location rf : toRemove)
-            rf.getParent().removeLocation(rf);
-        
+		Set<Location> toRemove = new HashSet<Location>();
+		toRemove.addAll(model.selectionModel().getLocationSelection());
+		model.selectionModel().clearLocationSelection();
+		for (Location rf : toRemove) {
+			Feature f = rf.getParent();
+			/* If there are more locations, remove the selected one */
+			if (f.location().size() > 1)
+				f.removeLocation(rf);
+			/* If this is the last location of the feature, remove the feature instead*/
+			else {
+				Data<?>d=model.getSelectedEntry().get(f.type());
+				if(d instanceof MemoryFeatureAnnotation){
+					MemoryFeatureAnnotation mf=(MemoryFeatureAnnotation)d;
+					mf.remove(f);
+				}
+				
+			}
 
-    }
+		}
 
-    public void update(Observable o, Object arg) {
-        SortedSet<Location> set = model.selectionModel().getLocationSelection();
-        setEnabled(set.size() > 0);
+	}
 
-    }
+	public void update(Observable o, Object arg) {
+		SortedSet<Location> set = model.selectionModel().getLocationSelection();
+		setEnabled(set.size() > 0);
+
+	}
 
 }
