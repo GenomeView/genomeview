bf04dae897ef1be68277186167b4c8676fb74dfb
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2011-02-03 00:17:21 +0000

    Further improved missing reference detection
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1364 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/ReferenceMissingMonitor.java
+++ net/sf/genomeview/gui/ReferenceMissingMonitor.java
@@ -1,48 +1,73 @@
+/**
+ * %HEADER%
+ */
 package net.sf.genomeview.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.EventQueue;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JScrollPane;
-import javax.swing.JWindow;
 import javax.swing.border.Border;
 
+import net.sf.genomeview.core.Icons;
 import net.sf.genomeview.data.Model;
 import net.sf.genomeview.gui.components.JEditorPaneLabel;
 import net.sf.genomeview.gui.menu.file.LoadFeaturesAction;
 import net.sf.jannot.DataKey;
 import net.sf.jannot.Entry;
 import net.sf.jannot.EntrySet;
-
-public class ReferenceMissingMonitor extends JWindow implements Observer {
+/**
+ * 
+ * @author Thomas Abeel
+ *
+ */
+public class ReferenceMissingMonitor extends JDialog implements Observer {
 
 	private static final long serialVersionUID = 566883531326807914L;
 	private JEditorPaneLabel floater = new JEditorPaneLabel();
 	private Model model;
 	private boolean dismissed = false;
+	private JScrollPane jp;
 
 	public ReferenceMissingMonitor(Model model) {
-		final JWindow _self = this;
+		super(model.getGUIManager().getParent(), ModalityType.MODELESS);
+		setTitle("Reference sequence missing!!");
+		setIconImage(Icons.MINILOGO);
+		addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				dismissed=true;
+				super.windowClosing(e);
+			}
+			
+		});
+		
+		final JDialog _self = this;
+
 		this.model = model;
 		model.addObserver(this);
 		model.getWorkerManager().addObserver(this);
 		Rectangle bounds = model.getGUIManager().getParent().getBounds();
-		// this.setBounds(bounds);
-		this.setAlwaysOnTop(true);
-		this.setPreferredSize(new Dimension(bounds.width /3, bounds.height / 5));
-		// floater.setHorizontalAlignment(SwingConstants.CENTER);
-		// floater.setVerticalAlignment(SwingConstants.CENTER);
-		floater.setAlignmentY(CENTER_ALIGNMENT);
+		
+		this.setPreferredSize(new Dimension(bounds.width / 3, bounds.height / 5));
+		
 		this.setLocation(bounds.x + bounds.width / 3, bounds.y + bounds.height / 5);
 
 		floater.setOpaque(true);
@@ -53,7 +78,9 @@ public class ReferenceMissingMonitor extends JWindow implements Observer {
 		floater.setBorder(BorderFactory.createCompoundBorder(colorBorder, emptyBorder));
 		floater.setBackground(new Color(255, 0, 0, 100));
 		setLayout(new BorderLayout());
-		add(new JScrollPane(floater), BorderLayout.CENTER);
+		jp=new JScrollPane(floater);
+		jp.getVerticalScrollBar().addAdjustmentListener(new ScrollFixer(jp));
+		add(jp, BorderLayout.CENTER);
 
 		Container buttons = new Container();
 		buttons.setLayout(new BorderLayout());
@@ -78,22 +105,39 @@ public class ReferenceMissingMonitor extends JWindow implements Observer {
 		pack();
 	}
 
-	private void setMsg(String text) {
-		floater.setText(text);
+	
 
+
+
+	private int lastMissing = -1;
+
+	class ScrollFixer implements AdjustmentListener{
+
+		private JScrollPane jp;
+
+		public ScrollFixer(JScrollPane jp) {
+			this.jp=jp;
+		}
+
+		@Override
+		public void adjustmentValueChanged(AdjustmentEvent e) {
+			if(!e.getValueIsAdjusting())
+				jp.repaint();
+			
+		}
+
+	
+		
 	}
-
-	private String getMsg() {
-		return floater.getText();
-	}
-
+	
 	@Override
 	public void update(Observable o, Object arg) {
+		final JDialog _self = this;
 		/* Data was reset */
-		if(dismissed&&model.entries().size()==0)
-			dismissed=false;
+		if (dismissed && model.entries().size() == 0)
+			dismissed = false;
 		/* When window was dismissed, we don't want to show it anymore */
-		if(dismissed)
+		if (dismissed)
 			return;
 		/* While there is still data loading, we wait */
 		if (model.getWorkerManager().runningJobs() > 0) {
@@ -109,6 +153,7 @@ public class ReferenceMissingMonitor extends JWindow implements Observer {
 		ArrayList<String> missing = new ArrayList<String>();
 		for (Entry e : es) {
 			if (e.sequence().size() == 0) {
+				//System.out.println(e.sequence().getClass());
 				int dataCount = 0;
 				for (DataKey dk : e)
 					dataCount++;
@@ -118,23 +163,39 @@ public class ReferenceMissingMonitor extends JWindow implements Observer {
 				}
 			}
 		}
-		// System.out.println("Missing refs: " + missingReference);
-		if (missingReference > 0) {
+		
+		if (missingReference > 0 && lastMissing != missingReference) {
 			StringBuffer msg = new StringBuffer(
 					"<html><h1>Warning!!</h1>Not every entry has a reference sequence loaded! Some visualizations will not work as expected without reference.<br><br>Entries without reference: ");
 			for (String s : missing) {
 				msg.append("\n" + s);
 			}
 			msg.append("</html>");
-			if (this.getMsg().length() != msg.length()) {
-				this.setMsg(msg.toString());
-				this.setVisible(true);
-			}
+			floater.setText(msg.toString());
+			/* Make dialog visible */
+			EventQueue.invokeLater(new Runnable() {
 
-		} else {
+				@Override
+				public void run() {
+					_self.setVisible(true);
+
+				}
+			});
+			/* Scroll to top */
+			EventQueue.invokeLater(new Runnable() {
+
+				@Override
+				public void run() {
+					jp.getVerticalScrollBar().setValue(0);
+
+				}
+			});
+
+		} else if(missingReference == 0){
 			this.setVisible(false);
 
 		}
+		lastMissing = missingReference;
 
 	}
 }
