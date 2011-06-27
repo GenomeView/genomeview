8ab6f69b4f1f225205ddf4859e3b727302f16c04
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2011-06-27 17:40:54 +0000

    properly handle the url qualifier
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1669 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/information/FeatureDetailPanel.java
+++ net/sf/genomeview/gui/information/FeatureDetailPanel.java
@@ -7,6 +7,7 @@ import java.awt.GridBagConstraints;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Arrays;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Set;
@@ -16,10 +17,13 @@ import javax.swing.JEditorPane;
 import javax.swing.JLabel;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
+import javax.swing.text.html.StyleSheet;
 
+import net.sf.genomeview.core.Colors;
 import net.sf.genomeview.core.Configuration;
 import net.sf.genomeview.data.Model;
 import net.sf.genomeview.gui.StaticUtils;
+import net.sf.genomeview.gui.components.JEditorPaneLabel;
 import net.sf.jannot.Feature;
 import be.abeel.gui.GridBagPanel;
 
@@ -32,17 +36,20 @@ import be.abeel.gui.GridBagPanel;
  */
 public class FeatureDetailPanel extends GridBagPanel implements Observer {
 
-	/**
-     * 
-     */
 	private static final long serialVersionUID = 1214531303733670258L;
 
-	private JEditorPane name = new JEditorPane();
+	private JEditorPaneLabel name = new JEditorPaneLabel();
 
 	private Model model;
 
 	public FeatureDetailPanel(Model model) {
 		this.model = model;
+
+		StyleSheet css = name.getStyleSheet();
+		css.addRule("body {color:#000; margin-left: 4px; margin-right: 4px; }");
+		css.addRule("p {margin:0px;padding:0px;}");
+		css.addRule("h3 {font-size:115%;color: " + Colors.encode(Configuration.green) + ";margin:0px;padding:0px;}");
+
 		name.setEditable(false);
 		name.addMouseListener(new MouseAdapter() {
 
@@ -76,8 +83,8 @@ public class FeatureDetailPanel extends GridBagPanel implements Observer {
 					popupMenu.add(wrapMenu(Query.google));
 					popupMenu.add(wrapMenu(Query.ncbiQuery));
 					popupMenu.add(wrapMenu(Query.ensemblQuery));
-					//popupMenu.add(wrapMenu(Query.ebi));
-					
+					// popupMenu.add(wrapMenu(Query.ebi));
+
 					popupMenu.add(wrapMenu(Query.plaza));
 
 					String extra = Configuration.get("extraqueries");
@@ -86,7 +93,7 @@ public class FeatureDetailPanel extends GridBagPanel implements Observer {
 						String[] arr = extra.split(";");
 						for (String s : arr) {
 							String[] tmp = s.split(",");
-							JMenuItem extramenu = wrapMenu(new Query(tmp[0], tmp[1],null));
+							JMenuItem extramenu = wrapMenu(new Query(tmp[0], tmp[1], null));
 							popupMenu.add(extramenu);
 						}
 					}
@@ -97,13 +104,13 @@ public class FeatureDetailPanel extends GridBagPanel implements Observer {
 			}
 
 			private JMenuItem wrapMenu(final Query q) {
-				return new JMenuItem(new AbstractAction(q.getLabel(),q.getIcon()) {
+				return new JMenuItem(new AbstractAction(q.getLabel(), q.getIcon()) {
 
 					private static final long serialVersionUID = -3208849232821620577L;
 
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						
+
 						q.query(name.getSelectedText());
 
 					}
@@ -111,32 +118,6 @@ public class FeatureDetailPanel extends GridBagPanel implements Observer {
 			}
 
 		});
-		// name.setEditorKit(new HTMLEditorKit());
-		// name.setDocument(new HTMLDocument());
-		// name.addHyperlinkListener(new HyperlinkListener() {
-		//
-		// @Override
-		// public void hyperlinkUpdate(HyperlinkEvent e) {
-		// if (e.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-		//
-		// System.out.println(e.getDescription());
-		// URL url = e.getURL();
-		// System.out.println("Hyperlink: " + url);
-		// // try {
-		// // Desktop.getDesktop().browse(url.toURI());
-		// // } catch (IOException e1) {
-		// // // TODO Auto-generated catch block
-		// // e1.printStackTrace();
-		// // } catch (URISyntaxException e1) {
-		// // // TODO Auto-generated catch block
-		// // e1.printStackTrace();
-		// // }
-		// //
-		// }
-		// }
-		//
-		// });
-		// ;
 
 		model.addObserver(this);
 		gc.fill = GridBagConstraints.BOTH;
@@ -157,28 +138,32 @@ public class FeatureDetailPanel extends GridBagPanel implements Observer {
 		Set<Feature> set = model.selectionModel().getFeatureSelection();
 		if (set != null && set.equals(lastSelection))
 			return;
-		String text = "";
+		StringBuffer text = new StringBuffer();
 
 		if (set != null && set.size() > 0) {
 			for (Feature rf : set) {
 				// text += "Data origin: " + rf.getSource() + "\n";
 				if (rf.location() != null)
-					text += "Location: " + StaticUtils.escapeHTML(rf.location().toString()) + "\n";
-				text += "Strand: " + rf.strand() + "\n";
-				text += "Score: " + rf.getScore() + "\n";
+					text.append("Location: " + StaticUtils.escapeHTML(Arrays.toString(rf.location())) + "<br/>");
+				text.append("Strand: " + rf.strand() + "<br/>");
+				text.append("Score: " + rf.getScore() + "<br/>");
 				Set<String> qks = rf.getQualifiersKeys();
 				for (String key : qks) {
-					
-						text += key+"="+rf.qualifier(key) + "\n";
-					
+					if (key.equals("url")) {
+						String[] urls = rf.qualifier(key).split(",");
+						for (String url : urls)
+							text.append("<a href='" + url + "'>" + url + "</a><br/>");
+					} else
+						text.append(key + "=" + rf.qualifier(key) + "<br/>");
+
 				}
 
 			}
 
-			text += "---------------------------------------\n";
+			// text += "---------------------------------------\n";
 			// int hash = text.hashCode();
 			// if (hash != lastHash) {
-			name.setText(text);
+			name.setText("<html><body>" + text + "</body></html>");
 			// name.scrollRectToVisible(new Rectangle(0, 0, 1, 1));
 			// lastHash = hash;
 			// }
