9215a6f15275f7b20b68738cdcde873694d28ada
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2011-08-16 15:09:10 +0000

    restructured code to avoid duplication
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1744 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/viztracks/comparative/MultipleAlignmentTrack2.java
+++ net/sf/genomeview/gui/viztracks/comparative/MultipleAlignmentTrack2.java
@@ -188,7 +188,7 @@ public class MultipleAlignmentTrack2 extends Track {
 			return 20 + 5;
 		}
 
-		if (estCount < 500) {
+		if (estCount < 250) {
 			int yMax = 0;
 			CollisionMap hitmap = new CollisionMap(model);
 			MouseHit mh = null;
@@ -232,31 +232,37 @@ public class MultipleAlignmentTrack2 extends Track {
 				BitSet lines = new BitSet(ordering.size());
 
 				/* Very detailed view */
+				char[] ref = null;
 				if (visible.length() < 1000) {
 					// char[] ref =
 					// entry.sequence().getSubSequence(visible.start,
 					// visible.end + 1).toCharArray();
 					Iterable<Character> bufferedSeq = entry.sequence().get(visible.start, visible.end + 1);
 
-					char[] ref = new char[visible.length()];
+					ref = new char[visible.length()];
 
 					int idx = 0;
 					for (char c : bufferedSeq) {
 						ref[idx++] = c;
 					}
-					// System.out.println("REF: "+Arrays.toString(ref));
-					// System.out.println("--block ");
-					int line = 1;
-					for (AbstractAlignmentSequence as : ab2) {
-						// System.out.println("\t" + start + "\t" + end + "\t" +
-						// as.strand());
-						// g.drawString(as.seq().toString(), x1,
-						// rec.y+line*lineHeight);
-						if (showAll) {
-							line = ordering.getForward(as.getName()) + 1;
-							lines.set(line - 1);
-						}
-						// System.out.println(as+"\t"+line);
+				}
+				// System.out.println("REF: "+Arrays.toString(ref));
+				// System.out.println("--block ");
+				int line = 1;
+				Font font = g.getFont();
+				Font tmpFont = font.deriveFont(10f);
+				g.setFont(tmpFont);
+				for (AbstractAlignmentSequence as : ab2) {
+					// System.out.println("\t" + start + "\t" + end + "\t" +
+					// as.strand());
+					// g.drawString(as.seq().toString(), x1,
+					// rec.y+line*lineHeight);
+					if (showAll) {
+						line = ordering.getForward(as.getName()) + 1;
+						lines.set(line - 1);
+					}
+					// System.out.println(as+"\t"+line);
+					if (visible.length() < 1000) {
 						for (int i = visible.start; i <= visible.end; i++) {
 							if (i >= start && i < end) {
 								double width = screenWidth / (double) visible.length();
@@ -293,13 +299,7 @@ public class MultipleAlignmentTrack2 extends Track {
 								}
 							}
 						}
-						line++;
-					}
-
-				} else {/* Not very much zoomed in, but still close */
-					int line = 1;
-
-					for (AbstractAlignmentSequence as : ab2) {
+					} else {
 						if (showAll) {
 							line = ordering.getForward(as.getName()) + 1;
 							lines.set(line - 1);
@@ -311,57 +311,50 @@ public class MultipleAlignmentTrack2 extends Track {
 						}
 
 						g.fillRect(x1, rec.y + (line - 1) * lineHeight, x2 - x1, lineHeight);
-
-						line++;
-					}
-
-				}
-
-				/* Paint annotation if present */
-				int lineIdx=1;
-				Font font=g.getFont();
-				Font tmpFont=font.deriveFont(10f);
-				g.setFont(tmpFont);
-				for (AbstractAlignmentSequence as : ab2) {
-//					System.out
-//							.println(as.getName() + " " + as.start() + "\t" + as.end() + "\t" + (as.end() > as.start()));
-					Entry e=model.entry(as.getName());
-					if(e!=null){
-//						System.out.println("\t"+e+"\t"+x1+"\t"+x2);
-						MemoryFeatureAnnotation mfa=e.getMemoryAnnotation(Type.get(Configuration.get("maf:annotationType")));
-						for(Feature f: mfa.get(as.start(), as.end())){
-//							System.out.println("\t\t"+f+"\t"+f.location()[0]);
-							double a=as.start();
-							double c=as.end();
-							double q=f.start();
-							double r=f.end();
-							if(as.strand()==Strand.REVERSE){
-								q=as.end()-f.end()+as.start();
-								r=as.end()-f.start()+as.start();
-//								System.out.println(f.start()+"\t"+f.end()+"\t"+q+"\t"+r);
-							}
-								
-							
-							double x=(((q-a)/(c-a))*(x2-x1))+x1;
-							double y=(((r-a)/(c-a))*(x2-x1))+x1;
-//							System.out.println("\t\t"+f+"\t"+f.location()[0]+"\t"+x+"\t"+y);
-							g.setColor(Color.CYAN);
-							if(x<x1)
-								x=x1;
-							if(y>x2)
-								y=x2;
-							
-							g.fillRect((int)x, rec.y + (lineIdx - 1) * lineHeight+3, (int)(y-x), lineHeight-6);
-							
-							g.setColor(Color.CYAN.darker().darker());
-							g.drawString(f.toString(), (int)x, rec.y + (lineIdx) * lineHeight-4);
-							
-						}
 						
+						
+						
+
 					}
-					lineIdx++;
 					
+					Entry e = model.entry(as.getName());
+					if (e != null) {
+						// System.out.println("\t"+e+"\t"+x1+"\t"+x2);
+						MemoryFeatureAnnotation mfa = e.getMemoryAnnotation(Type.get(Configuration
+								.get("maf:annotationType")));
+						for (Feature f : mfa.get(as.start(), as.end())) {
+							// System.out.println("\t\t"+f+"\t"+f.location()[0]);
+							double a = as.start();
+							double c = as.end();
+							double q = f.start();
+							double r = f.end();
+							if (as.strand() == Strand.REVERSE) {
+								q = as.end() - f.end() + as.start();
+								r = as.end() - f.start() + as.start();
+								// System.out.println(f.start()+"\t"+f.end()+"\t"+q+"\t"+r);
+							}
+							/* These are only estimates of the right coordinates */
+							double x = (((q - a) / (c - a)) * (x2 - x1)) + x1;
+							double y = (((r - a) / (c - a)) * (x2 - x1)) + x1;
+							// System.out.println("\t\t"+f+"\t"+f.location()[0]+"\t"+x+"\t"+y);
+							g.setColor(Color.CYAN);
+							if (x < x1)
+								x = x1;
+							if (y > x2)
+								y = x2;
+
+							g.fillRect((int) x, rec.y + (line - 1) * lineHeight + 3, (int) (y - x), lineHeight - 6);
+
+							g.setColor(Color.CYAN.darker().darker());
+							g.drawString(f.toString(), (int) x, rec.y + (line) * lineHeight - 4);
+
+						}
+
+					}
+					
+					line++;
 				}
+
 				g.setFont(font);
 
 				/* Fill in the blanks when showing all */
