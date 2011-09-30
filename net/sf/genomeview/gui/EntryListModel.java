916546f9219b5545c492f5474c834ff1f8b24da1
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2011-09-30 16:34:21 +0000

    entries are ordered naturally
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1798 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/EntryListModel.java
+++ net/sf/genomeview/gui/EntryListModel.java
@@ -4,14 +4,20 @@
 package net.sf.genomeview.gui;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.PriorityQueue;
 import java.util.Set;
 
 import javax.swing.ComboBoxModel;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
+
+import be.abeel.util.NaturalOrderComparator;
 
 import net.sf.genomeview.data.DummyEntry;
 import net.sf.genomeview.data.Model;
@@ -35,6 +41,7 @@ final class EntryListModel implements Observer, ComboBoxModel {
 
 	private int lastSize = 0;
 
+	private List<Entry>tmpList=new ArrayList<Entry>();
 	@Override
 	public Object getElementAt(int i) {
 		if(tmpList.size()==0||i>=tmpList.size())
@@ -58,20 +65,28 @@ final class EntryListModel implements Observer, ComboBoxModel {
 		model.setSelectedEntry((Entry) anItem);
 
 	}
-	private ArrayList<Entry>tmpList=new ArrayList<Entry>();
+	
+	private class EntryComparator implements Comparator<Entry>{
+
+		private Comparator<String> noc=NaturalOrderComparator.NUMERICAL_ORDER;
+		@Override
+		public int compare(Entry o1, Entry o2) {
+			return noc.compare(o1.getID(), o2.getID());
+		}
+		
+	}
+	
 
 	@Override
 	public synchronized void update(Observable arg0, Object arg1) {
-		// System.out.println("loop");
 		int size = model.noEntries();
-		// for (int i = 0; i < size; i++)
-		// System.out.println("\t" + getElementAt(i));
-		// System.out.println("fire: " + model.noEntries());
 		if (size != lastSize) {
-			tmpList.clear();
+			ArrayList<Entry>tmp=new ArrayList<Entry>();
 			for(Entry e:model.entries()){
-				tmpList.add(e);
+				tmp.add(e);
 			}
+			Collections.sort(tmp,new EntryComparator());
+			tmpList=tmp;
 			ListDataEvent le = new ListDataEvent(this, ListDataEvent.CONTENTS_CHANGED, 0, model.noEntries());
 			for (ListDataListener l : listeners) {
 				l.contentsChanged(le);
