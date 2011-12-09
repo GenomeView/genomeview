0c1899731651cf43305e947056691b57ecb684a3
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2011-12-09 18:44:37 +0000

    added support for remote images
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1855 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/core/Icons.java
+++ net/sf/genomeview/core/Icons.java
@@ -5,16 +5,21 @@ package net.sf.genomeview.core;
 
 import java.awt.Image;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+
 /**
  * 
  * @author Thomas Abeel
- *
+ * 
  */
 public class Icons {
-	//FIXME fix with get method
+	// FIXME fix with get method
 	public static final Icon DELETE = new ImageIcon(Icons.class.getResource("/images/delete.png"));
 
 	public static final Icon YES = new ImageIcon(Icons.class.getResource("/images/yes.png"));
@@ -22,9 +27,9 @@ public class Icons {
 	public static final Icon NO = new ImageIcon(Icons.class.getResource("/images/no.png"));
 	public static final Icon BDASH = new ImageIcon(Icons.class.getResource("/images/bdash.png"));
 
-	public static final Icon VISIBLE=new ImageIcon(Icons.class.getResource("/images/visible.png"));
-	public static final Icon INVISIBLE=new ImageIcon(Icons.class.getResource("/images/invisible.png"));
-	
+	public static final Icon VISIBLE = new ImageIcon(Icons.class.getResource("/images/visible.png"));
+	public static final Icon INVISIBLE = new ImageIcon(Icons.class.getResource("/images/invisible.png"));
+
 	public static final Icon EDIT = new ImageIcon(Icons.class.getResource("/images/edit.png"));
 
 	public static final Icon DOWN_ARROW = new ImageIcon(Icons.class.getResource("/images/downarrow.png"));
@@ -41,19 +46,28 @@ public class Icons {
 
 	public static Icon LOGO = new ImageIcon(Icons.class.getResource("/images/logo.png"));
 
-	public static Icon LARGEOPEN=get("boxopen.png");
-	
-	public static Icon MINILOGO_ICON=get("gv2.png");
-	
-	public static Image MINILOGO=get("gv2.png").getImage();
+	public static Icon LARGEOPEN = get("boxopen.png");
+
+	public static Icon MINILOGO_ICON = get("gv2.png");
+
+	public static Image MINILOGO = get("gv2.png").getImage();
 
 	private Icons() throws IOException {
 
 	}
 
+	private static Logger log=Logger.getLogger(Icons.class.getCanonicalName());
+	
 	public static ImageIcon get(String string) {
-		return new ImageIcon(Icons.class.getResource("/images/"+string));
+		if (string.startsWith("http"))
+			try {
+				return new ImageIcon(new URL(string));
+			} catch (MalformedURLException e) {
+				log.log(Level.SEVERE,"Problem while loading image from URL: "+string,e);
+				return null;
+			}
+		else
+			return new ImageIcon(Icons.class.getResource("/images/" + string));
 	}
 
 }
-
