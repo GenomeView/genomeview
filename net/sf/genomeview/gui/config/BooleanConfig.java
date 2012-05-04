d54237a6c56efce84339410eb35e52baef9c62f9
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2012-05-04 16:06:57 +0000

    +public, added docs
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@1885 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- net/sf/genomeview/gui/config/BooleanConfig.java
+++ net/sf/genomeview/gui/config/BooleanConfig.java
@@ -1,5 +1,22 @@
 /**
- * %HEADER%
+ * This file is part of GenomeView, a genome browser and annotation curator
+ * 
+ * Copyright (C) 2012 Thomas Abeel
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ *
+ * Project: http://genomeview.org/
  */
 package net.sf.genomeview.gui.config;
 
@@ -9,8 +26,12 @@ import java.awt.event.ActionListener;
 import javax.swing.JCheckBox;
 
 import net.sf.genomeview.core.Configuration;
-
-class BooleanConfig extends JCheckBox {
+/**
+ * 
+ * @author Thomas Abeel
+ *
+ */
+public class BooleanConfig extends JCheckBox {
 
 	private static final long serialVersionUID = 9081788377933556296L;
 
