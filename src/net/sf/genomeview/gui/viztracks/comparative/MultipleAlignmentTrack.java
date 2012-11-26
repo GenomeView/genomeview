ac73634e66ef57f5e4b6849291ca80d5e585e75a
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2012-11-26 19:45:28 +0000

    percent identify plot
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@2032 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- src/net/sf/genomeview/gui/viztracks/comparative/MultipleAlignmentTrack.java
+++ src/net/sf/genomeview/gui/viztracks/comparative/MultipleAlignmentTrack.java
@@ -12,6 +12,7 @@ import java.awt.geom.AffineTransform;
 import java.awt.geom.GeneralPath;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -36,75 +37,77 @@ import net.sf.jannot.alignment.mfa.AlignmentAnnotation;
  * The multiple alignment visualization for multi-fasta files
  */
 public class MultipleAlignmentTrack extends Track {
-	private String name;
-	double LOG2 = Math.log(2);
-	int bareScale = 32;
-	int bareScaleIndex = 5;
-	private Map<Entry, Buffer> buffers = new HashMap<Entry, Buffer>();
+//	private String name;
+	
+	
+//	double LOG2 = Math.log(2);
+//	int bareScale = 32;
+//	int bareScaleIndex = 5;
+	private Map<Alignment, float[]> pips = new HashMap<Alignment, float[]>();
 
-	class Buffer {
-		private Alignment a;
-
-		Buffer(Alignment a) {
-			this.a = a;
-		}
-
-		private List<float[]> buffer = new ArrayList<float[]>();
-
-		public double get(int start, int scale) {
-
-			if (scale < bareScale) {
-				double conservation = 0;
-				for (int j = 0; j < scale; j++) {
-					if (a.isAligned(start + j))
-						conservation++;
-				}
-				return conservation / scale;
-			}
-			if (buffer.size() == 0)
-				buffer.add(bare());
-
-			int index = (int) (Math.log(scale) / LOG2) - bareScaleIndex;
-
-			while (buffer.size() <= index + 1) {
-				buffer.add(merge(buffer.get(buffer.size() - 1)));
-			}
-
-			return buffer.get(index)[start / scale];
-		}
-
-		private float[] merge(float[] ds) {
-			float[] out = new float[(ds.length + 1) / 2];
-			for (int i = 0; i < ds.length - 1; i += 2) {
-				out[i / 2] = (ds[i] + ds[i + 1]) / 2;
-			}
-			out[out.length - 1] = ds[ds.length - 1];
-			return out;
-		}
-
-		private float[] bare() {
-
-			float[] out = new float[a.refLength() / bareScale + 1];
-			for (int i = 0; i < a.refLength(); i += bareScale) {
-				float conservation = 0;
-				for (int j = 0; j < bareScale; j++) {
-					if (a.isAligned(i + j))
-						conservation++;
-				}
-				conservation /= bareScale;
-				out[i / bareScale] = conservation;
-
-			}
-			return out;
-		}
-
-	}
+//	class Buffer {
+//		private Alignment a;
+//
+//		Buffer(Alignment a) {
+//			this.a = a;
+//		}
+//
+//		private List<float[]> buffer = new ArrayList<float[]>();
+//
+//		public double get(int start, int scale) {
+//
+//			if (scale < bareScale) {
+//				double conservation = 0;
+//				for (int j = 0; j < scale; j++) {
+//					if (a.isAligned(start + j))
+//						conservation++;
+//				}
+//				return conservation / scale;
+//			}
+//			if (buffer.size() == 0)
+//				buffer.add(bare());
+//
+//			int index = (int) (Math.log(scale) / LOG2) - bareScaleIndex;
+//
+//			while (buffer.size() <= index + 1) {
+//				buffer.add(merge(buffer.get(buffer.size() - 1)));
+//			}
+//
+//			return buffer.get(index)[start / scale];
+//		}
+//
+//		private float[] merge(float[] ds) {
+//			float[] out = new float[(ds.length + 1) / 2];
+//			for (int i = 0; i < ds.length - 1; i += 2) {
+//				out[i / 2] = (ds[i] + ds[i + 1]) / 2;
+//			}
+//			out[out.length - 1] = ds[ds.length - 1];
+//			return out;
+//		}
+//
+//		private float[] bare() {
+//
+//			float[] out = new float[a.refLength() / bareScale + 1];
+//			for (int i = 0; i < a.refLength(); i += bareScale) {
+//				float conservation = 0;
+//				for (int j = 0; j < bareScale; j++) {
+//					if (a.isAligned(i + j))
+//						conservation++;
+//				}
+//				conservation /= bareScale;
+//				out[i / bareScale] = conservation;
+//
+//			}
+//			return out;
+//		}
+//
+//	}
 
 	private MultipleAlignmentTrackModel mat = null;
 
 	public MultipleAlignmentTrack(Model model, DataKey key) {
 		super(key, model, true, false);
-		this.name = key.toString();
+//		this.name = key.toString();
 		AlignmentAnnotation entireAlignment = (AlignmentAnnotation) entry
 				.get(dataKey);
 		mat = new MultipleAlignmentTrackModel(entireAlignment);
@@ -116,42 +119,42 @@ public class MultipleAlignmentTrack extends Track {
 //		return "MA: " + name;
 //	}
 
-	static class Cache {
-		private int cacheStart = -1;
-		private int cacheEnd = -1;
-		private int cacheScale = 1;
-		private double[] cacheValues = null;
-
-		public boolean hasData(int scale, int start, int end) {
-			return scale == cacheScale && start >= cacheStart
-					&& end <= cacheEnd;
-		}
-
-		public void store(int scale, int start, int end, double[] cacheValues2) {
-			this.cacheScale = scale;
-			this.cacheStart = start;
-			this.cacheEnd = end;
-			this.cacheValues = cacheValues2;
-
-		}
-
-		public double[] get() {
-			return cacheValues;
-		}
-
-		public int start() {
-			return cacheStart;
-		}
-
-		public int end() {
-			return cacheEnd;
-		}
-
-		public int scale() {
-			return cacheScale;
-		}
-
-	}
+//	static class Cache {
+//		private int cacheStart = -1;
+//		private int cacheEnd = -1;
+//		private int cacheScale = 1;
+//		private double[] cacheValues = null;
+//
+//		public boolean hasData(int scale, int start, int end) {
+//			return scale == cacheScale && start >= cacheStart
+//					&& end <= cacheEnd;
+//		}
+//
+//		public void store(int scale, int start, int end, double[] cacheValues2) {
+//			this.cacheScale = scale;
+//			this.cacheStart = start;
+//			this.cacheEnd = end;
+//			this.cacheValues = cacheValues2;
+//
+//		}
+//
+//		public double[] get() {
+//			return cacheValues;
+//		}
+//
+//		public int start() {
+//			return cacheStart;
+//		}
+//
+//		public int end() {
+//			return cacheEnd;
+//		}
+//
+//		public int scale() {
+//			return cacheScale;
+//		}
+//
+//	}
 
 	class MultipleAlignmentTrackModel {
 
@@ -309,41 +312,59 @@ public class MultipleAlignmentTrack extends Track {
 			} else {
 
 				for (Alignment alignment : mat.ordered()) {
-					double width = screenWidth / (double) r.length() / 5.0;
+//					double width = screenWidth / (double) r.length() / 5.0;
 
-					int scale = 1;
-					while (scale < (int) Math.ceil(1.0 / width))
-						scale *= 2;
+//					int scale = 1;
+//					while (scale < (int) Math.ceil(1.0 / width))
+//						scale *= 2;
 
-					GeneralPath conservationGP = new GeneralPath();
-					conservationGP.moveTo(0, yOffset);
-
-					int start = r.start() / scale * scale;
-					int end = ((r.end() / scale) + 1) * scale;
+//					GeneralPath conservationGP = new GeneralPath();
+//					conservationGP.moveTo(0, yOffset);
+//
+//					int start = r.start() / scale * scale;
+//					int end = ((r.end() / scale) + 1) * scale;
 
 					// /* Plot whatever is in the cache */
-					if (!buffers.containsKey(entry))
-						buffers.put(entry, new Buffer(alignment));
-					Buffer b = buffers.get(entry);
+					if (!pips.containsKey(alignment))
+						pips.put(alignment, percentIdentify(alignment));
+					float[] b = pips.get(alignment);
 
-					for (int i = 0; i < (end - start) / scale; i++) {
-						int x = Convert.translateGenomeToScreen(start + i
-								* scale, r, screenWidth) + 5;
-
-						conservationGP.lineTo(x,
-								yOffset + (1 - b.get(start + i * scale, scale))
-										* (lineHeigh - 4) + 2);
+					int stepSize=(int)Math.max(1,Math.floor((r.length()/screenWidth)/2));
+					
+					for (int i = r.start; i <=r.end; i+=stepSize) {
+						int x = Convert.translateGenomeToScreen(i, r, screenWidth);
+						int width= Math.max(1,Convert.translateGenomeToScreen(i+stepSize, r, screenWidth)-x);
+						float min=1;
+						float max=0;
+						for(int j=0;j<stepSize;j++){
+							float val=b[i+j-1];
+							if(val>max)
+								max=val;
+							if(val<min)
+								min=val;
+								
+						}
+						int height=(int)Math.max(1,lineHeigh*(max-min));
+						int y = (int)(max*lineHeigh);
+						g.setColor(Color.BLACK);
+//						System.out.println("Plotting pips: "+x+"\t"+y);
+						g.fillRect(x, yOffset+lineHeigh-y,width, height);
+//						conservationGP.lineTo(x,
+//								yOffset + (1 - b.get(start + i * scale, scale))
+//										* (lineHeigh - 4) + 2);
 					}
-					g.setColor(Color.BLACK);
-					g.draw(conservationGP);
-					g.setColor(Color.BLUE);
-					g.drawString(alignment.name() + " (" + scale + ")", 10,
-							yOffset + lineHeigh - 2);
+					
+//					g.draw(conservationGP);
+//					g.setColor(Color.BLUE);
+//					g.drawString(alignment.name() + " (" + scale + ")", 10,
+//							yOffset + lineHeigh - 2);
 					yOffset += lineHeigh;
 				}
 
 			}
 			int logoLineHeight = 40;
+			
+			
 			if (model.getAnnotationLocationVisible().length() < 100) {
 				double width = screenWidth / (double) r.length();
 				int grouping = (int) Math.ceil(1.0 / width);
@@ -412,6 +433,38 @@ public class MultipleAlignmentTrack extends Track {
 
 		}
 		return 0;
+	}
+
+	private float[] percentIdentify(Alignment alignment) {
+		BitSet rollingBuffer=new BitSet();
+		
+		int reflen=alignment.refLength();
+		for(int i=1;i<=reflen;i++){
+			if(alignment.getReferenceNucleotide(i)==alignment.getNucleotide(i))
+				rollingBuffer.set(i-1);
+		}
+		float[] out=new float[reflen];
+		for(int i=0;i<reflen;i++){
+			int count=0;
+			for(int j=-50;j<=50;j++){
+				/* wrap index */
+				int idx=(i-j);
+				if(idx<0)
+					idx+=reflen;
+				if(idx>=reflen)
+					idx-=reflen;
+				if(rollingBuffer.get(idx))
+					count++;
+				
+			}
+			
+			out[i]=(float)(count/101.0);
+			if(count!=101)
+			System.out.println("calculate: "+count+"\t"+out[i]);
+		}
+		return out;
+		
+		
 	}
 
 	@Override
