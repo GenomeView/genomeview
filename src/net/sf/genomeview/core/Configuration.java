3ff201912c63b9677d40422ec4610750b37c1951
thomasabeel <thomasabeel@6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce>
2013-03-11 15:49:13 +0000

    load additional synonyms on start-up
    git-svn-id: svn://svn.code.sf.net/p/genomeview/code/genomeview/trunk@2113 6512a8d4-c03c-0410-a19d-ba0f7ff4f5ce

--- src/net/sf/genomeview/core/Configuration.java
+++ src/net/sf/genomeview/core/Configuration.java
@@ -23,8 +23,11 @@ import net.sf.jannot.Type;
 import net.sf.jannot.parser.EMBLParser;
 import net.sf.jannot.parser.GFF3Parser;
 import net.sf.jannot.parser.Parser;
+import net.sf.jannot.source.Locator;
+import net.sf.nameservice.NameService;
 import be.abeel.io.GZIPPrintWriter;
 import be.abeel.io.LineIterator;
+import be.abeel.net.URIFactory;
 
 /**
  * Low level access to the configuration.
@@ -43,19 +46,21 @@ public class Configuration {
 	}
 
 	public static char[] getAminoAcids() {
-		return new char[] { 'M', '*', 'X', 'Y', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'N', 'L', 'K', 'I', 'H', 'G',
-				'F', 'E', 'D', 'C', 'A' };
+		return new char[] { 'M', '*', 'X', 'Y', 'W', 'V', 'U', 'T', 'S', 'R',
+				'Q', 'P', 'N', 'L', 'K', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'A' };
 
 	}
 
-	private static Logger logger = Logger.getLogger(Configuration.class.getCanonicalName());
+	private static Logger logger = Logger.getLogger(Configuration.class
+			.getCanonicalName());
 
 	static {
 		String s = System.getProperty("user.home");
 		confDir = new File(s + "/.genomeview");
 		if (!confDir.exists()) {
 			if (!confDir.mkdir())
-				logger.warning("Could not create configuration directory: " + confDir);
+				logger.warning("Could not create configuration directory: "
+						+ confDir);
 
 		}
 		logger.info("User config: " + confDir);
@@ -82,7 +87,8 @@ public class Configuration {
 			 */
 			save();
 		} catch (IOException e) {
-			CrashHandler.crash(Level.SEVERE, "IOException while loading configuration", e);
+			CrashHandler.crash(Level.SEVERE,
+					"IOException while loading configuration", e);
 
 		}
 	}
@@ -107,7 +113,8 @@ public class Configuration {
 	private static void load() throws IOException {
 		InputStream is = null;
 		try {
-			is = Configuration.class.getResourceAsStream("/genomeview.properties");
+			is = Configuration.class
+					.getResourceAsStream("/genomeview.properties");
 			gvProperties.load(is);
 		} catch (Exception e1) {
 			logger.warning("genomeview.properties file could not be loaded! GenomeView assumes your are a developer and know why you can ignore this.");
@@ -122,7 +129,8 @@ public class Configuration {
 		logger.info("Loading default configuration...");
 		LineIterator it;
 
-		it = new LineIterator(Configuration.class.getResourceAsStream("/conf/default.conf"));
+		it = new LineIterator(
+				Configuration.class.getResourceAsStream("/conf/default.conf"));
 		it.setSkipBlanks(true);
 		it.setSkipComments(true);
 		for (String line : it) {
@@ -145,7 +153,8 @@ public class Configuration {
 			logger.warning("Config file has size zero!");
 		} else {
 			try {
-				it = new LineIterator(new GZIPInputStream(new FileInputStream(configFile)));
+				it = new LineIterator(new GZIPInputStream(new FileInputStream(
+						configFile)));
 				it.setSkipBlanks(true);
 				it.setSkipComments(true);
 				for (String line : it) {
@@ -154,14 +163,34 @@ public class Configuration {
 						String value = line.substring(line.indexOf('=') + 1);
 						localMap.put(key.trim(), value.trim());
 					} else {
-						logger.warning("Invalid line in configuration file! '" + line + "'");
+						logger.warning("Invalid line in configuration file! '"
+								+ line + "'");
 					}
 
 				}
 				it.close();
 			} catch (Exception e) {
-				logger.log(Level.SEVERE, "Something went horribly wrong while loading the configuration.", e);
+				logger.log(
+						Level.SEVERE,
+						"Something went horribly wrong while loading the configuration.",
+						e);
 			}
+		}
+		try {
+			for (String s : get("synonyms").split(",")) {
+
+				try {
+					NameService.addSynonyms(URIFactory.url(s).openStream());
+				} catch (Exception e) {
+					logger.log(Level.WARNING,"Failed to load additional synonyms for: "+s,e);
+				}
+
+			}
+
+		} catch (Exception e) {
+			logger.log(Level.WARNING,
+					"Failed to load additional synonyms for option: "
+							+ get("synonyms"),e);
 		}
 
 	}
@@ -311,7 +340,8 @@ public class Configuration {
 		try {
 			load();
 		} catch (IOException e) {
-			CrashHandler.crash(Level.SEVERE, "IOException while loading configuration", e);
+			CrashHandler.crash(Level.SEVERE,
+					"IOException while loading configuration", e);
 		}
 		model.refresh();
 	}
@@ -353,8 +383,8 @@ public class Configuration {
 	}
 
 	public static boolean getVisible(DataKey dk) {
-		if (get("track:visible:" + dk) == null){
-			set("track:visible:" + dk,true);
+		if (get("track:visible:" + dk) == null) {
+			set("track:visible:" + dk, true);
 			return true;
 		}
 		return getBoolean("track:visible:" + dk);
