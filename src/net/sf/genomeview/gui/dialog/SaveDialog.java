094c0e48c4975ab85a05301b4c56efdf194d0b92
Thomas Abeel <thomas@abeel.be>
2013-09-05 15:30:16 -0400

    remove removed method

--- src/net/sf/genomeview/gui/dialog/SaveDialog.java
+++ src/net/sf/genomeview/gui/dialog/SaveDialog.java
@@ -300,9 +300,7 @@ public class SaveDialog extends JDialog {
 									((EMBLParser) parser).storeSequence = true;
 
 							}
-							if (parser instanceof GFF3Parser){
-								((GFF3Parser)parser).setAssureIDs(true);
-							}
+							
 							File tmp = File.createTempFile("GV_", ".save");
 							tmp.deleteOnExit();
 
