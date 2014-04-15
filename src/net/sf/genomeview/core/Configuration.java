e15374830ba34416541fc9356c689e87bce958d5
Thomas Abeel <thomas@abeel.be>
2014-04-15 16:30:55 -0400

    removed unused imports

--- src/net/sf/genomeview/core/Configuration.java
+++ src/net/sf/genomeview/core/Configuration.java
@@ -17,8 +17,6 @@ import java.util.Properties;
 import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import java.util.zip.GZIPInputStream;
 
 import net.sf.genomeview.data.Model;
