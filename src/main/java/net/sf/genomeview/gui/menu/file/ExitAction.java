be6046c827993859133ad7d811ebfe9211e09579
Wouter <nomail@hotmail.com>
2026-02-16 14:26:46 +0100

    ctrl-Q for exit

--- src/main/java/net/sf/genomeview/gui/menu/file/ExitAction.java
+++ src/main/java/net/sf/genomeview/gui/menu/file/ExitAction.java
@@ -5,21 +5,23 @@ package net.sf.genomeview.gui.menu.file;
 
 import java.awt.event.ActionEvent;
 
+import javax.swing.KeyStroke;
+
 import net.sf.genomeview.data.Model;
 import net.sf.genomeview.gui.MessageManager;
 import net.sf.genomeview.gui.menu.AbstractModelAction;
 
-
 public class ExitAction extends AbstractModelAction {
 
-    private static final long serialVersionUID = 5781369935758205711L;
+	private static final long serialVersionUID = 5781369935758205711L;
 
-    public ExitAction(Model model) {
-        super(MessageManager.getString("filemenu.exit"), model);
-    }
+	public ExitAction(Model model) {
+		super(MessageManager.getString("filemenu.exit"), model);
+		super.putValue(ACCELERATOR_KEY, KeyStroke.getKeyStroke("control Q"));
+	}
 
-    public void actionPerformed(ActionEvent arg0) {
-        model.exit();
-    }
+	public void actionPerformed(ActionEvent arg0) {
+		model.exit();
+	}
 
 }
